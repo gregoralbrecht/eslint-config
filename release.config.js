21b7ec331ad3c477c1d518a039d7b32c20180929
Gregor Albrecht <hello@gregoralbrecht.com>
2020-06-16 20:25:36 +0200

    New: Plugin to publish to NPM registry

--- release.config.js
+++ release.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   branches: ["main"],
-  plugins: ["@semantic-release/commit-analyzer"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+  ],
   preset: "eslint",
 };
